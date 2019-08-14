708f0169327f9d06e4d3427e5a61e0c08c642ee1
Juha Lento <juha.lento@csc.fi>
2019-08-14 14:36:21 +0300

    User-space Spack setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # myspack
+
 Use cases for user-space Spack chained to system Spack
+
+## Setup
+
+Source <myspack.sh>:
+
+```bash
+source myspack.sh
+```
+
---
+++ myspack.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+source <(sed '1,/^__FUNCS__/d' ${BASH_SOURCE[0]})
+
+# Set up user-space spack enviromenment that is chained to system spack
+
+export SPACK_ROOT=$(cd $(dirname ${BASH_SOURCE[0]});pwd)/spack
+export SPACK_UPSTREAM_ROOT=/appl/spack/spack
+export SPACK_UPSTREAM_INSTALL_TREE=/appl/spack/install-tree
+
+myspack-create-git-repository \
+    $SPACK_ROOT https://github.com/spack/spack.git || return
+myspack-configure \
+    $SPACK_ROOT $SPACK_UPSTREAM_ROOT $SPACK_UPSTREAM_INSTALL_TREE || return
+
+source $SPACK_ROOT/share/spack/{setup-env,spack-completion}.sh
+
+return
+
+
+__FUNCS__
+
+myspack-create-git-repository () {
+    local usage="
+        Usage: ${FUNCNAME[0]} MYSPACK_ROOT MYSPACK_REPOSITORY
+        "
+    local spackroot=${1:?$usage}
+    local spackurl=${2:?$usage}
+    mkdir -p $(dirname $spackroot)
+    echo "${spackroot}:"
+    ( cd $spackroot && \
+	git remote update && \
+	git status -uno || \
+	git clone $spackurl $spackroot
+    ) | sed 's/^/    /' 
+}
+
+myspack-configure () {
+    local usage="
+        Usage ${FUNCNAME[0]} SPACK_ROOT UPSTREAM_ROOT [UPSTREAM_INSTALL_TREE]
+        "
+    local spackroot=${1:?$usage}
+    local upstreamroot=${2:?$usage}
+    local upstreamtree=${3:-$2/opt/spack}
+    echo > $spackroot/etc/spack/upstreams.yaml "
+        upstreams:
+            spack-instance-1:
+                install_tree: $upstreamtree
+        "
+    cat $upstreamroot/etc/spack/repos.yaml \
+	<(echo "  - $(dirname $spackroot)/repo)") \
+	> $spackroot/etc/spack/repos.yaml
+    ln -sf \
+	$upstreamroot/etc/spack/{compilers,packages}.yaml \
+	$spackroot/etc/spack
+}
